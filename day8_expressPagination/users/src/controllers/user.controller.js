d4a0a480e4774e32adf42a83c74ee799b4f04c91
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-03-24 00:32:38 +0530

    File Upload

--- day8_expressPagination/users/src/controllers/user.controller.js
+++ day8_expressPagination/users/src/controllers/user.controller.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const User = require("../models/user.model");
 const { body, validationResult } = require('express-validator');
+const {upload} = require("../utils")
+
 const router = express.Router()
+// const upload = multer({ dest: `${__dirname}/../../uploads`})
+
+
 
 
 router.get("/", async(req,res)=>{
@@ -14,11 +19,13 @@ router.get("/", async(req,res)=>{
     res.status(200).json({data:users,totalPages})
 })
 router.post("/", 
-    body('first_name').isLength({min:3,max:10}).withMessage("FN is required and minimum and max length is 3 and 10"),
-    body('last_name').isLength({min:3,max:10}).withMessage("FN is required and minimum and max length is 3 and 10"),
-    body('age').isFloat({min:18,max:100}).withMessage("age be in between 18 to 100"),
-    body('email').isEmail().withMessage("Check Email"),
+    // body('first_name').isLength({min:3,max:10}).withMessage("FN is required and minimum and max length is 3 and 10"),
+    // body('last_name').isLength({min:3,max:10}).withMessage("FN is required and minimum and max length is 3 and 10"),
+    // body('age').isFloat({min:18,max:100}).withMessage("age be in between 18 to 100"),
+    // body('email').isEmail().withMessage("Check Email"),
+    upload.array("avatar"),
     async(req,res)=>{
+     console.log(req.body);   
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -26,8 +33,9 @@ router.post("/",
     try{
         let currentCount = await User.find().countDocuments()
         let createUser = await User.create({
+            ...req.body,
             id:currentCount+1,
-            ...req.body
+            avatar:req.files.map((f) => f.path),
         });
         res.status(200).json(createUser)
     }catch(e){
@@ -36,3 +44,6 @@ router.post("/",
 })
 
 module.exports =router;
+
+
+//1.04
