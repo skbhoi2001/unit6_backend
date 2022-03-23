529efad702bd8d2f4daf323c699e62ed5809446e
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2022-03-23 14:33:56 +0530

    Express validation

--- day8_expressPagination/users/src/controllers/user.controller.js
+++ day8_expressPagination/users/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user.model");
+const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
 
@@ -12,10 +13,23 @@ router.get("/", async(req,res)=>{
     let totalPages = Math.ceil((await User.find().countDocuments())/pageSize);
     res.status(200).json({data:users,totalPages})
 })
-router.post("/", async(req,res)=>{
+router.post("/", 
+    body('first_name').isLength({min:3,max:10}).withMessage("FN is required and minimum and max length is 3 and 10"),
+    body('last_name').isLength({min:3,max:10}).withMessage("FN is required and minimum and max length is 3 and 10"),
+    body('age').isFloat({min:18,max:100}).withMessage("age be in between 18 to 100"),
+    body('email').isEmail().withMessage("Check Email"),
+    async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     try{
-        let createUser = await User.create(req.body);
-    res.status(200).json(createUser)
+        let currentCount = await User.find().countDocuments()
+        let createUser = await User.create({
+            id:currentCount+1,
+            ...req.body
+        });
+        res.status(200).json(createUser)
     }catch(e){
         res.send(400).send(e.message)
     }
--- day8_expressPagination/users/src/models/user.model.js
+++ day8_expressPagination/users/src/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
+    id:{type:Number,required:true},
     first_name:{type:String,required:true},
     last_name:{type:String,required:true},
     email:{type:String,required:true},
